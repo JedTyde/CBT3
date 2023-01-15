f61c98191f935f7752f3abbf8d38a8792dca13e6
JedTyde <59925703+JedTyde@users.noreply.github.com>
2023-01-15 19:27:12 +0100

    HA BROMA
    mira te lo digo enserio si termino matando a alguien o me mato te juro por mi puta vida que no sera la forma mas bonita

--- WiwaEditor/src/WiwaApp.cpp
+++ WiwaEditor/src/WiwaApp.cpp
@@ -5,6 +5,8 @@
 #include <Wiwa/ecs/systems/System.h>
 #include <Wiwa/scripting/ScriptEngine.h>
 #include <Wiwa/core/Renderer2D.h>
+//#include <Wiwa/utilities/ui/ManagerUI.h>
+//#include <Wiwa/utilities/ui/ButtonUI.h>
 
 USE_REFLECTION;
 class WiwaApp : public Wiwa::Application
@@ -31,9 +33,17 @@ public:
 		Wiwa::Image* img = Wiwa::Resources::GetResourceById<Wiwa::Image>(imgId);
 		Wiwa::Vector2i position = {960,540};
 		Wiwa::Size2i size = img->GetSize()/4;
-		Wiwa::Rect2i rect = { 0,0,img->GetWidth()/2,img->GetHeight()/2};
+		Wiwa::Rect2i rect = { 0,0,img->GetWidth(),img->GetHeight()};
 		Wiwa::Color4f color = Wiwa::Color::WHITE;
 		Wiwa::Application::Get().GetRenderer2D().CreateInstancedQuadTex(imgId, position, size, color, rect, Wiwa::Renderer2D::Pivot::CENTER);
+
+		/*Wiwa::Button* but;
+		ResourceId imgSam = Wiwa::Resources::Load<Wiwa::Image>("Assets/sam.jpg");
+		Wiwa::Image * simg = Wiwa::Resources::GetResourceById<Wiwa::Image>(imgSam);
+		Wiwa::Vector2i position2 = { 960,540 };
+		Wiwa::Size2i size2 = simg->GetSize();
+		Wiwa::Rect2i rect2 = { 0,0,simg->GetWidth(),simg->GetHeight() };
+		but = Wiwa::ManagerUi::CreateControlUi(Wiwa::UiType::BUTTON, Wiwa::ButtonType::PLAY, position2, simg, rect2);*/
 	}
 
 	~WiwaApp()
